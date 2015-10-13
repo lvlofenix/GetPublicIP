a8bb6aaef257d22b0b0125dbe7dd7ed5e6c871c7
Lucas Vinicius <fenix@hotmail.com.br>
2015-10-13 11:26:36 -0300

    1.1 Beta
    Beta

---
+++ GetPublicIP/GetPublicIP.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net;
+using System.IO;
+
+namespace GetPublicIP
+{
+    public class GetPublicIP
+    {
+        public string getIP()
+        {
+            try
+            {
+                String publicIP = String.Empty;
+                WebRequest request = WebRequest.Create(@"http://checkip.dyndns.org");
+                using (WebResponse response = request.GetResponse())
+                using (StreamReader stream = new StreamReader(response.GetResponseStream()))
+                { publicIP = stream.ReadToEnd();}
+                int ini = publicIP.IndexOf("Address: ") + 9;
+                int fim = publicIP.LastIndexOf("</body>");
+                publicIP = publicIP.Substring(ini, fim - ini);
+                return publicIP;
+            }
+            catch
+            {
+                return "404";
+            }
+        }
+    }
+}
